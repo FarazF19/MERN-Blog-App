8f73965e26d70abe8531ee5cbff273acfb2bae3d
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-09-20 15:26:11 +0500

    created pages and routes

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
